5c9d920eec966383a9ff5025aecd211e13017750
Optykan <syang0525@gmail.com>
2018-01-06 12:41:41 -0500

    add files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # site-boilerplate
 Foundation6 + Sass + Gulp.js + Browserify + Express.js + Babel for easy site setup and less pain in the future. maybe consider setting up React or something later
+
+
+# Setup
+
+Clone the repo, rename it, then `npm install`. `npm run` to start the server
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var fs          = require('fs');
+var gulp        = require('gulp');
+var child       = require('child_process');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var notify      = require('gulp-notify');
+var babel       = require('gulp-babel');
+var babelcore   = require('babel-core');
+var autoprefixer= require('gulp-autoprefixer');
+
+// Static Server + watching scss/html files
+
+gulp.task('server', ['sass'], function() {
+	var server = child.spawn('node', ['bin/www']);
+	var log = fs.createWriteStream('server.log', {flags: 'a'});
+	server.stderr.pipe(log);
+
+	browserSync.init({
+		proxy: "localhost:3000",
+		port: 3001,
+		notify: true
+	});
+
+	gulp.watch("assets/scss/*.scss", ['sass']);
+	gulp.watch("assets/js/**/*.js", ['js']);
+	gulp.watch(["routes/*.html", "views/**/*.ejs"]).on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+	return gulp.src("assets/scss/*.scss")
+	.pipe(sass({ outputStyle: 'compressed' })
+		.on('error', sass.logError))
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascase: false
+	}))
+	.pipe(gulp.dest("public/stylesheets"))
+	.pipe(browserSync.stream())
+	.pipe(notify("Compiled successfully"));
+});
+
+gulp.task('js', function(){
+	gulp.src("assets/js/**/*.js")
+	.pipe(babel({
+		presets: ['env']
+	}).on('error', console.log))
+	.pipe(gulp.dest("public/javascripts"));
+
+	return browserSync.reload();
+})
+
+gulp.task('default', ['server']);
